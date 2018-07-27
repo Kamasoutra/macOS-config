2fb3a16424891ed4a4108e19acca79b701824721
Jean-Pierre Palik <kama@palik.fr>
2018-07-27 14:53:15 +0200

    added gitignore and custom cask for sketch

---
+++ custom_cask/sketch.rb
@@ -0,0 +1,24 @@
+cask 'sketch' do
+  version '43.2'
+  sha256 '4a67e22b0f0794f8ccc2be169236b7d5baf0f8a02082dcb3d4e2a41fd2ad4c13'
+
+  url "https://download.sketchapp.com/sketch-#{version}.zip"
+  appcast 'https://download.sketchapp.com/sketch-versions.xml'
+  name 'Sketch'
+  homepage 'https://www.sketchapp.com/'
+
+  auto_updates false
+
+  app 'Sketch.app'
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*',
+               '~/Library/Application Support/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3',
+               '~/Library/Logs/com.bohemiancoding.sketch3',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.plist',
+               '~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies',
+             ]
+end
